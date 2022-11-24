c8da9b45466b61808a69cef38054495e66f9a531
rodridu <96802506+rodridu@users.noreply.github.com>
2022-11-24 22:22:47 +0800

    Update combine_data.py

--- codes/combine_data.py
+++ codes/combine_data.py
@@ -1,8 +1,4 @@
-'''
-author: zhimingmei
-date: 27 Jul, 2022
-description: 将生成的所有预测数据合并到同一张大表上
-'''
+
 
 from distutils import filelist
 from doctest import OutputChecker
@@ -10,8 +6,8 @@ import pandas as pd
 import os
 
 def merge_csv():
-    input_path = r'/Users/ryan/Documents/GitHub/GDHF-Quant-Investment/test_data/predict_data/'
-    output_path = r'/Users/ryan/Documents/GitHub/GDHF-Quant-Investment/test_data/'
+    input_path = r'/Users/hongz/test_data/predict_data/'
+    output_path = r'/Users/hongz/test_data/'
     result_name = 'merged_table.csv'
 
     os.chdir(input_path)
